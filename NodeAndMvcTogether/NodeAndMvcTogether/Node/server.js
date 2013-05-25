1f2346871fabd2cb430b834eabbc45adcfcdbea3
buhbuh <JuggleDrum@hotmail.co.uk>
2013-05-25 15:26:47 +0100

    Add the node http server

---
+++ NodeAndMvcTogether/NodeAndMvcTogether/Node/server.js
@@ -0,0 +1,11 @@
+﻿//This is a very simple http server.
+
+var http = require('http');
+var port = process.env.PORT || 1337;
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+
+}).listen(poert);
