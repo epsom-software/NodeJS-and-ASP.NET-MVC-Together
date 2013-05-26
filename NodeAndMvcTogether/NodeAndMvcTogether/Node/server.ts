92b45f659946525a956f614b878905068e1497bf
buhbuh <JuggleDrum@hotmail.co.uk>
2013-05-26 18:05:20 +0100

    Convert the server,js file to TypeScript

--- NodeAndMvcTogether/NodeAndMvcTogether/Node/server.ts
+++ NodeAndMvcTogether/NodeAndMvcTogether/Node/server.ts
@@ -1,11 +1,13 @@
 ///<reference path='Definitions\node.d.ts' />
+
 //This is a very simple http server.
+
 var http = require('http');
 var port = process.env.PORT || 1337;
+
 http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World from Node.js and TypeScript\n');
+
 }).listen(port);
-//@ sourceMappingURL=server.js.map
